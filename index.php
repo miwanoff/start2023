ed2d22bd69ec74dfa1470834973465e368ea4d41
miwanoff <marina.iva.noffa@gmail.com>
2023-09-03 14:52:55 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>first</title>
+</head>
+
+<body>
+    <h1><?php
+
+$str1 = "Hello,";
+$str2 = "PHP!";
+echo $str1 . $str2;
+
+?></h1>
+    <a href="sum.php">Sum</a>
+</body>
+
+</html>
