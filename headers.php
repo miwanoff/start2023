c5e64ea1f5b36b5f6cda51c80bd57a3c8c0c5ecd
miwanoff <marina.iva.noffa@gmail.com>
2023-09-09 15:49:23 +0300

    Add headers

---
+++ headers.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="uk">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Площа кола</title>
+</head>
+
+<body>
+    <h2>3.3 Площа кола</h2>
+    <p>Площа - <b>$s</b></p>
+    <?php
+$n = 5;
+for ($i = 1; $i <= $n; $i++) {
+    echo "\t<h$i>Заголовок $i</h$i>\n";
+}
+?>
+
+</body>
+
+</html>
