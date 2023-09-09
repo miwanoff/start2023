5ebd403add384ab6d54515ccc3560ff664f3db48
miwanoff <marina.iva.noffa@gmail.com>
2023-09-09 15:20:39 +0300

    Add prompt for temp and circle

--- tepmerature.php
+++ tepmerature.php
@@ -1,28 +1,26 @@
 <?php
-$name = "Температура";
-$begin = <<<EOD
-<!DOCTYPE html>
-<html>
+$r = rand(5, 500);
+$s = round(M_PI * $r ** 2);
 
+$start = <<<EOD
+<!DOCTYPE html>
+<html lang="uk">
 <head>
     <meta charset="UTF-8">
-    <title>$name</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Площа кола</title>
 </head>
 <body>
-  <h1>$name</h1>\n
+<h2>3.3 Площа кола</h2>
+<p>Площа - <b>$s</b></p>
 EOD;
-echo $begin;
+echo $start;
+$r = rand(50, 200);
+echo "<div style=\"background-color: lightblue; border-radius: 50%; width: 200px; height: 200px;\"></div>";
+echo "<div style=\"background-color: lightblue; border-radius: 50%; width: ".(2 * $r)."px; height: ".(2 * $r)."px;\"></div>";
+
 $end = <<<EOD
 </body>
 </html>
 EOD;
-
-$x = rand(-30, 50); //-10.5;
-$x = (int) $x;
-if ($x < 0) {
-    echo "  <p style=\"color:blue;\"> $x - Мороз </p>";
-} else {
-    echo $x;
-}
 echo $end;
-// $x < 0?..:...; 
